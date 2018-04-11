f70781703dae447f0281a81330ec8e1ff65c30e7
Matthew Crooks <mfcrooks@gmail.com>
2018-04-11 01:36:58 +0000

    Add index.erb to /views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "golfclubsaregreat"
+  end
+
   get '/' do
-    "Hello, World!"
+    erb :index
   end
 end
