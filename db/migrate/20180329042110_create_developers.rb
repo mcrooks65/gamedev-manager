b01fb3c03c49c8482e193eb2824ff62d8310601a
Matthew Crooks <mfcrooks@gmail.com>
2018-04-13 22:20:22 +0000

    Rm unneccesary 'games' column from dev

--- db/migrate/20180329042110_create_developers.rb
+++ db/migrate/20180329042110_create_developers.rb
@@ -2,7 +2,6 @@ class CreateDevelopers < ActiveRecord::Migration
   def change
     create_table :developers do |t|
       t.string :name
-      t.string :games
       t.string :location
       t.integer :employees
     end
