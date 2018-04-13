a4c0c24c3fd4b6e713bd57b71ec76a2762bac765
Matthew Crooks <mfcrooks@gmail.com>
2018-04-13 07:07:49 +0000

    Improved seeds.rb working dev_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
-Developer.create(name: "RogueWare", password: "test")
-Game.create(title: "FreeHolder")
+@rogueware = Developer.create(name: "RogueWare", password: "test", location:"Los Angeles, CA", employees: 2)
+Game.create(title: "FreeHolder", developer: @rogueware, price: 9.99, genre: "strategy/rpg/roguelike", description: "
+Blending strategy, survival, and roguelike elements with RPG style character development, FreeHolder is the story of three escaped slaves in the time of the Roman Republic. Farm, hunt, craft, and cook your way through the years as you live on the edge of the beginning of the Empire.")
