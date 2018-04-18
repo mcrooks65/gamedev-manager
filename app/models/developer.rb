560b4c12157968d1f3050aae7e6b04ef94878767
Matthew Crooks <mfcrooks@gmail.com>
2018-04-18 01:44:10 +0000

    Add slugify methods to developer.rb

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,4 +1,13 @@
 class Developer < ActiveRecord::Base
   has_many :games
   has_secure_password
+
+  def slug
+    name.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Developer.all.find{|dev| dev.slug == slug}
+  end
+
 end
