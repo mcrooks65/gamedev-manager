1cd975552104a23daf2f0085085e0adbc3a7ad62
Matthew Crooks <mfcrooks@gmail.com>
2018-04-18 02:56:35 +0000

    Add slugify methods to game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,11 @@
 class Game < ActiveRecord::Base
   belongs_to :developer
+
+  def slug
+    title.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Game.all.find{|game| game.slug == slug}
+  end
 end
