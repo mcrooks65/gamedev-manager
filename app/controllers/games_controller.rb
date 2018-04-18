df4097a37aab97b978eb5028f96548faaaa7f0c3
Matthew Crooks <mfcrooks@gmail.com>
2018-04-18 03:23:48 +0000

    Update show.erb and games_controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,4 +3,14 @@ class GamesController < ApplicationController
     @games = Game.all
     erb :'/games/index'
   end
+
+  get '/games/:slug' do # Game Show page using slugs
+    @current_game = Game.find_by_slug(params[:slug])
+    if @current_game
+      @current_game_dev = Developer.find { |dev| dev.id == @current_game.developer_id }
+      erb :'games/show'
+    else
+      erb :error2 #might want a new error message for game not found..
+    end
+  end
 end
