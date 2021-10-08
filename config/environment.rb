def5908ee8d8f412fa229a503f15630d5f1b35f3
mcrooks65 <mfcrooks@gmail.com>
2021-10-08 16:36:57 -0700

    Fix for ActiveRecord::AdapterNotSpecified error

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,11 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+
+set :database, {adapter: "sqlite3", database: "db/development.db"} 
 
 require_all 'app'
